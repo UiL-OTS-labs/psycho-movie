6b6e1680696bff8c47250324435104dc646b5305
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2018-03-19 17:28:16 +0100

    Add docstring to function

--- movie_part.py
+++ movie_part.py
@@ -3,6 +3,7 @@ import stimuli
 import settings
 
 def run_movie_part(window, stimuli):
-    
+    ''' This runs the movie part of the experiment.
+    '''
     for params in stimuli:
         movie = params.filename
