d12690a43a9fc98ad76a3163b31c9c06e600a942
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2018-03-28 17:27:25 +0200

    Add forgotten new module, oops :-(

---
+++ uilutils/silence_psychopy.py
@@ -0,0 +1,50 @@
+'''
+Psychopy does good work. However, a number of imported modules log to the
+terminal. The annoying truth about this is that important messages are missed.
+This module tries to make psychopy (or its modules quiet).
+'''
+
+
+import os.path
+
+
+class _suppress_stdout_stderr(object):
+    '''
+    A context manager for doing a "deep suppression" of stdout and stderr in
+    Python, i.e. will suppress all print, even if the print originates in a
+    compiled C/Fortran sub-function.
+       This will not suppress raised exceptions, since exceptions are printed
+    to stderr just before a script exits, and after the context manager has
+    exited (at least, I think that is why it lets exceptions through).
+
+    This class is taken from stackoverflow:
+    https://stackoverflow.com/questions/11130156/suppress-stdout-stderr-print-from-python-functions
+    '''
+    def __init__(self):
+        # Open a pair of null files
+        self.null_fds = [os.open(os.devnull,os.O_RDWR) for x in range(2)]
+        # Save the actual stdout (1) and stderr (2) file descriptors.
+        self.save_fds = [os.dup(1), os.dup(2)]
+
+    def __enter__(self):
+        # Assign the null pointers to stdout and stderr.
+        os.dup2(self.null_fds[0],1)
+        os.dup2(self.null_fds[1],2)
+
+    def __exit__(self, *_):
+        # Re-assign the real stdout/stderr back to (1) and (2)
+        os.dup2(self.save_fds[0],1)
+        os.dup2(self.save_fds[1],2)
+        # Close all file descriptors
+        for fd in self.null_fds + self.save_fds:
+            os.close(fd)
+
+def silence_psychopy():
+    '''Import psychopy but ignores all kinds of error messages from modules
+    psychopy imports. It is a good idea to turn this function off if
+    you encounter errors.
+    '''
+    with _suppress_stdout_stderr() as quiet:
+        from psychopy import visual, core, logging
+
+    logging.console = logging.LogFile("./error-log.txt", filemode='a')
